339f974757d544908a11ccf118fffce2638ac73f
jimjimvalkema <jjvalkema@protonmail.com>
2025-08-09 17:34:17 +0200

    cleanup, fixes and made work around with creating a point with a nonce that exceeds the field limit

--- README.md
+++ README.md
@@ -12,3 +12,8 @@ aztec-nargo compile;
 aztec codegen -o src/artifacts target;
 cd ../../..
 ```
+
+test + compile
+```shell
+cd contracts/aztec/AccountBasedPrivacy; aztec-nargo compile; aztec codegen -o src/artifacts target; cd ../../..; yarn test
+```
--- test/test.test.ts
+++ test/test.test.ts
@@ -1,8 +1,49 @@
-import { AztecAddress, Contract, createPXEClient, waitForPXE, Fr, GrumpkinScalar, type PXE, AccountWalletWithSecretKey, AztecAddressLike } from '@aztec/aztec.js';
+import { AztecAddress, Contract, createPXEClient, waitForPXE, Fr, GrumpkinScalar, type PXE, AccountWalletWithSecretKey, AztecAddressLike, FieldLike } from '@aztec/aztec.js';
 import { getDeployedTestAccountsWallets } from '@aztec/accounts/testing';
 import { AccountBasedPrivacyContract } from '../contracts/aztec/AccountBasedPrivacy/src/artifacts/AccountBasedPrivacy';
-const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+import { pedersenCommit } from '@aztec/foundation/crypto'
+import { toBufferBE , toBigIntBE} from 'bigint-buffer';
+import { ethers } from 'ethers';
+import { Grumpkin } from '@aztec/foundation/crypto';
+import { Point } from '@aztec/foundation/fields';
 
+const MAX_FIELD_BLINDING_POINT = await pedersenOnInt([0n, Fr.MAX_FIELD_VALUE.toBigInt()])
+
+async function pedersenOnInt(input:bigint[]) {
+    const commitment = await pedersenCommit(input.map((v)=>(new Fr(v)).toBuffer()))
+    const point1 = new Point(
+        Fr.fromBuffer(commitment[0]),
+        Fr.fromBuffer(commitment[1]),
+        false
+    );
+    return point1
+}
+
+
+async function createBlindingPoint(blindingNonce: bigint) {
+    if (blindingNonce < Fr.MAX_FIELD_VALUE.toBigInt()) {
+        return await pedersenOnInt([0n,blindingNonce])
+    } else {
+        const timesExceededLimit = blindingNonce / Fr.MAX_FIELD_VALUE.toBigInt()
+        const exceededLimitScalar = new GrumpkinScalar(timesExceededLimit)
+        const grumpkin = new Grumpkin()
+        const maxBlindingPointMul = await grumpkin.mul(MAX_FIELD_BLINDING_POINT, exceededLimitScalar)
+        const remainder = blindingNonce % Fr.MAX_FIELD_VALUE.toBigInt()
+        const remainderPoint = await pedersenOnInt([0n, remainder])
+        const blindingPoint = await grumpkin.add(maxBlindingPointMul,remainderPoint)
+        console.log(
+        "debug",    
+        {
+            timesExceededLimit,
+            remainder,
+            blindingNonce,
+            reproducedBlindingNonce: timesExceededLimit*Fr.MAX_FIELD_VALUE.toBigInt() + remainder
+        })
+        return blindingPoint
+    }
+}
+
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 describe("accountBasedPrivacy", () => {
     let wallets: AccountWalletWithSecretKey[]
     let PXE: PXE
@@ -15,18 +56,20 @@ describe("accountBasedPrivacy", () => {
     it("should spend", async () => {
         const walletDeployer = wallets[0] as AccountWalletWithSecretKey
         const walletAlice = wallets[1] as AccountWalletWithSecretKey
+        const viewingKeyAlice = 1234n
         const walletBob = wallets[2] as AccountWalletWithSecretKey
         const accountBasedPrivacy = await AccountBasedPrivacyContract.deploy(walletDeployer).send().deployed()
 
-        for (let amount = 0; amount < 3; amount++) {
+        for (let amount = 0; amount < 10; amount++) {
             await accountBasedPrivacy.withWallet(walletAlice).methods.mint(
                 walletAlice.getAddress(),
-                BigInt(amount),
+                BigInt(amount+100),
             ).send().wait()
         }
         const privateEvents = await PXE.getPrivateEvents(accountBasedPrivacy.address, AccountBasedPrivacyContract.events.PrivateIncomingTransfer, 0, 100000, [walletAlice.getAddress()])
         // TODO make issue getPublicEvents need to filter per contract address like getPrivateEvents does
         const publicEvents = await PXE.getPublicEvents(AccountBasedPrivacyContract.events.PublicIncomingTransfer, 0, 100000);
+        const aliceReceivedAmountBlockNumber = await PXE.getBlockNumber()
         //console.log({publicLenght: publicEvents.length, privateLenght: privateEvents.length})
         // because getPublicEvents doesn't filter on the contract address. Work around here assumes that the contract used to scan events is the most recent contract deployed
         const publicEventsOverShoot = publicEvents.length - privateEvents.length
@@ -39,10 +82,48 @@ describe("accountBasedPrivacy", () => {
         // yeah no filtering indexed events in aztec.js yet grrrrr!
         // see thread issue: https://github.com/AztecProtocol/aztec-packages/issues/16245
         const combinedEventsAlice = combinedEvents.filter((v: any) => AztecAddress.fromBigInt(v.recipient).equals(walletAlice.getAddress()))
-        const aliceReceivedAmountData = combinedEventsAlice.reduce((p: any, c: any, i) => {
-            return { receivedAmount: p.receivedAmount += c.amount, receivedAmountBlindingNonce: p.receivedAmountBlindingNonce += c.amount_blinding_nonce }
-        }, { receivedAmount: 0n, receivedAmountBlindingNonce: 0n })
-        console.log({aliceReceivedAmountData: aliceReceivedAmountData})
 
+        const aliceReceivedAmountData = {receivedAmount: 0n, receivedAmountBlindingNonce: 0n }
+        for (const {amount, amount_blinding_nonce} of combinedEventsAlice as any) {
+            aliceReceivedAmountData.receivedAmount += amount//% 
+            aliceReceivedAmountData.receivedAmountBlindingNonce += amount_blinding_nonce
+        }
+        // aliceReceivedAmountData.receivedAmount = aliceReceivedAmountData.receivedAmount % Fr.MODULUS
+        // aliceReceivedAmountData.receivedAmountBlindingNonce = aliceReceivedAmountData.receivedAmountBlindingNonce % Fr.MODULUS
+
+        console.log({ aliceReceivedAmountData: aliceReceivedAmountData })
+        console.log({combinedEventsAlice})
+        const blindingPoint = await createBlindingPoint(aliceReceivedAmountData.receivedAmountBlindingNonce)
+        const amountPoint = await pedersenOnInt([aliceReceivedAmountData.receivedAmount, 0n])
+        const grumpkin = new Grumpkin()
+        const receivedAmountPointBlinded = await grumpkin.add(amountPoint, blindingPoint)
+        console.log({ pedersenInJs_x: ethers.toBeHex(receivedAmountPointBlinded.x.toBigInt()) })
+        const current_point =  await accountBasedPrivacy.methods.get_received_amount(walletAlice.getAddress()).simulate()
+        console.log({current_point_x_from_chain: ethers.toBeHex(current_point.inner.x)} )
+        const blindingPointFormatted = {
+                x: blindingPoint.x.toBigInt(),
+                y:  blindingPoint.y.toBigInt(),
+                is_infinite: blindingPoint.isInfinite,
+        }
+        console.log({
+            recipient: walletBob.getAddress(),
+            amount:2n,
+            accountNonceIsZero: true,
+            viewingKeyAlice,
+            receivedAmount: aliceReceivedAmountData.receivedAmount,
+            blindingPointFormatted,
+            aliceReceivedAmountBlockNumber
+        })
+        const tx = await accountBasedPrivacy.withWallet(walletAlice).methods.transfer(
+            walletBob.getAddress(),
+            2n,
+            true,
+            viewingKeyAlice,
+            aliceReceivedAmountData.receivedAmount,
+            blindingPointFormatted,
+            aliceReceivedAmountBlockNumber
+        ).send().wait()
+
+        console.log({ tx })
     })
 });
